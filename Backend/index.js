707feb0e9d288051824e5ea25c672ef6ba1f837b
Shanto57575 <shanto57575@gmail.com>
2024-05-27 13:02:57 +0600

    done

--- Backend/index.js
+++ Backend/index.js
@@ -1,35 +1,39 @@
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import connectDB from './src/db/connectDB.js';
 import userRouter from './src/routes/user.routes.js';
 import recipeRouter from './src/routes/recipe.routes.js';
-import { errorHandler } from './src/middlewares/errorHandler.js'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import { errorHandler } from './src/middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import paymentRouter from './src/routes/payment.routes.js';
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(cors({
+    origin: 'https://reciperealm75.web.app',
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: "Dishcraft api is working !" })
-})
+});
 
-app.use('/api/user', userRouter)
-app.use('/api/recipe', recipeRouter)
-app.use('/api/payment', paymentRouter)
+app.use('/api/user', userRouter);
+app.use('/api/recipe', recipeRouter);
+app.use('/api/payment', paymentRouter);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`DishCraft Server is running on port ${port}`)
-})
+    console.log(`DishCraft Server is running on port ${port}`);
+});
